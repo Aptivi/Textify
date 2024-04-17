f626d0e623928ea9c3896b416234fb52f12c8da5
Aptivi <eoflaoevicecity@gmail.com>
2024-04-17 23:32:54 +0300

    add - Added scaffolding for braille builder
    ---
    
    We've added scaffolding for the braille builder. This is going to be useful for accessibility purposes.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Part: 1/1

---
+++ Textify/Accessibility/BrailleDots.cs
@@ -0,0 +1,56 @@
+﻿//
+// Textify  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Textify
+//
+// Textify is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Textify is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Textify.Accessibility
+{
+    /// <summary>
+    /// Braille dot enumeration
+    /// </summary>
+    public enum BrailleDots
+    {
+        /// <summary>
+        /// No dot. (⠀)
+        /// </summary>
+        None = 0,
+        /// <summary>
+        /// First dot, located in the first column, first row. (⠁)
+        /// </summary>
+        First = 1,
+        /// <summary>
+        /// Second dot, located in the first column, second row. (⠂)
+        /// </summary>
+        Second = 2,
+        /// <summary>
+        /// Third dot, located in the first column, third row. (⠄)
+        /// </summary>
+        Third = 4,
+        /// <summary>
+        /// Fourth dot, located in the second column, first row. (⠈)
+        /// </summary>
+        Fourth = 8,
+        /// <summary>
+        /// Fifth dot, located in the second column, second row. (⠐)
+        /// </summary>
+        Fifth = 16,
+        /// <summary>
+        /// Sixth dot, located in the second column, third row. (⠠)
+        /// </summary>
+        Sixth = 32,
+    }
+}
---
+++ Textify/Accessibility/BrailleMap.cs
@@ -0,0 +1,75 @@
+﻿//
+// Textify  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Textify
+//
+// Textify is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Textify is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Textify.Accessibility
+{
+    internal static class BrailleMap
+    {
+        internal static (string, string, BrailleDots)[] brailleMap =
+        [
+            ("", "", (BrailleDots)(-1)),
+            ("⠀", " ", BrailleDots.None),
+            ("⠁", "a", BrailleDots.First),
+            ("⠃", "b", BrailleDots.First | BrailleDots.Second),
+            ("⠉", "c", BrailleDots.First | BrailleDots.Fourth),
+            ("⠙", "d", BrailleDots.First | BrailleDots.Fourth | BrailleDots.Fifth),
+            ("⠑", "e", BrailleDots.First | BrailleDots.Fifth),
+            ("⠋", "f", BrailleDots.First | BrailleDots.Second | BrailleDots.Fourth),
+            ("⠛", "g", BrailleDots.First | BrailleDots.Second | BrailleDots.Fourth | BrailleDots.Fifth),
+            ("⠓", "h", BrailleDots.First | BrailleDots.Second | BrailleDots.Fifth),
+            ("⠊", "i", BrailleDots.Second | BrailleDots.Fourth),
+            ("⠚", "j", BrailleDots.Second | BrailleDots.Fourth | BrailleDots.Fifth),
+            ("⠅", "k", BrailleDots.First | BrailleDots.Third),
+            ("⠇", "l", BrailleDots.First | BrailleDots.Second | BrailleDots.Third),
+            ("⠍", "m", BrailleDots.First | BrailleDots.Third | BrailleDots.Fourth),
+            ("⠝", "n", BrailleDots.First | BrailleDots.Third | BrailleDots.Fourth | BrailleDots.Fifth),
+            ("⠕", "o", BrailleDots.First | BrailleDots.Third | BrailleDots.Fifth),
+            ("⠏", "p", BrailleDots.First | BrailleDots.Second | BrailleDots.Third | BrailleDots.Fourth),
+            ("⠟", "q", BrailleDots.First | BrailleDots.Second | BrailleDots.Third | BrailleDots.Fourth | BrailleDots.Fifth),
+            ("⠗", "r", BrailleDots.First | BrailleDots.Second | BrailleDots.Third | BrailleDots.Fifth),
+            ("⠎", "s", BrailleDots.Second | BrailleDots.Third | BrailleDots.Fourth),
+            ("⠞", "t", BrailleDots.Second | BrailleDots.Third | BrailleDots.Fourth | BrailleDots.Fifth),
+            ("⠥", "u", BrailleDots.First | BrailleDots.Third | BrailleDots.Sixth),
+            ("⠧", "v", BrailleDots.First | BrailleDots.Second | BrailleDots.Third | BrailleDots.Sixth),
+            ("⠺", "w", BrailleDots.Second | BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Sixth),
+            ("⠭", "x", BrailleDots.First | BrailleDots.Third | BrailleDots.Fourth | BrailleDots.Sixth),
+            ("⠽", "y", BrailleDots.First | BrailleDots.Third | BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Sixth),
+            ("⠵", "z", BrailleDots.First | BrailleDots.Third | BrailleDots.Fifth | BrailleDots.Sixth),
+            ("⠼", "#", BrailleDots.Third | BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Sixth),
+            ("⠂", ",", BrailleDots.Second),
+            ("⠆", ";", BrailleDots.Second | BrailleDots.Third),
+            ("⠒", ":", BrailleDots.Second | BrailleDots.Fifth),
+            ("⠲", ".", BrailleDots.Second | BrailleDots.Fifth | BrailleDots.Sixth),
+            ("⠦", "?", BrailleDots.Second | BrailleDots.Third | BrailleDots.Sixth),
+            ("⠖", "!", BrailleDots.Second | BrailleDots.Third | BrailleDots.Fifth),
+            ("⠄", "'", BrailleDots.Third),
+            ("⠄⠦", "‘", BrailleDots.Third | BrailleDots.Second | BrailleDots.Third | BrailleDots.Sixth),
+            ("⠄⠴", "’", BrailleDots.Third | BrailleDots.Second | BrailleDots.Third | BrailleDots.Fifth),
+            ("⠘⠦", "“", BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Second | BrailleDots.Third | BrailleDots.Sixth),
+            ("⠘⠴", "”", BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Second | BrailleDots.Third | BrailleDots.Fifth),
+            ("⠐", "(", BrailleDots.Fourth),
+            ("⠐", ")", BrailleDots.Third | BrailleDots.Fourth),
+            ("⠸⠌", "/", BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Sixth | BrailleDots.Third | BrailleDots.Fourth),
+            ("⠸⠡", "\\", BrailleDots.Fourth | BrailleDots.Fifth | BrailleDots.Sixth | BrailleDots.First | BrailleDots.Sixth),
+            ("⠤", "‐", BrailleDots.Third | BrailleDots.Sixth),
+            ("⠠⠤", "–", BrailleDots.Sixth | BrailleDots.Third | BrailleDots.Sixth),
+            ("⠐⠠⠤", "—", BrailleDots.Fifth | BrailleDots.Sixth | BrailleDots.Third | BrailleDots.Sixth),
+        ];
+    }
+}
