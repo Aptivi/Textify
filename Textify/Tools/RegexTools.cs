34443b44cdd3f6a93ef8cc26202e6ccb992b6de2
Aptivi CEO <eoflaoevicecity@gmail.com>
2024-08-25 20:15:09 +0300

    chg - Fixed FTBFS
    ---
    
    Type: chg
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Textify/Tools/RegexTools.cs
+++ Textify/Tools/RegexTools.cs
@@ -17,6 +17,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
 
 namespace Textify.Tools
