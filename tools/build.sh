67c2113e38611211a77a7af47d426538ef3f69e9
Aptivi <eoflaoevicecity@gmail.com>
2024-10-13 10:10:30 +0300

    imp - Rolled out updated build scripts
    ---
    
    The build scripts have been updated.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+#   Textify  Copyright (C) 2023-2024  Aptivi
+#  
+#   This file is part of Textify
+#  
+#   Textify is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#  
+#   Textify is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY, without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#  
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Convenience functions
+checkerror() {
+    if [ $1 != 0 ]
+    then
+        printf "$2 - Error $1\n" >&2
+        exit $1
+    fi
+}
+
 # This script builds. Use when you have dotnet installed.
 releaseconf=$1
 if [ -z $releaseconf ]; then
@@ -8,27 +34,17 @@ fi
 
 # Check for dependencies
 dotnetpath=`which dotnet`
-if [ ! $? == 0 ]; then
-	echo dotnet is not found.
-	exit 1
-fi
+checkerror $? "dotnet is not found"
 
 # Download packages
 echo Downloading packages...
-"$dotnetpath" restore "../Textify.sln" -p:Configuration=$releaseconf
-if [ ! $? == 0 ]; then
-	echo Download failed.
-	exit 1
-fi
+"$dotnetpath" restore "../Textify.sln" -p:Configuration=$releaseconf ${@:2}
+checkerror $? "Failed to download packages"
 
-# Build Textify
-echo Building Textify...
-"$dotnetpath" build "../Textify.sln" -p:Configuration=$releaseconf
-if [ ! $? == 0 ]; then
-	echo Build failed.
-	exit 1
-fi
+# Build
+echo Building...
+"$dotnetpath" build "../Textify.sln" -p:Configuration=$releaseconf ${@:2}
+checkerror $? "Failed to build Textify"
 
 # Inform success
 echo Build successful.
-exit 0
