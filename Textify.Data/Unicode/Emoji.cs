8a7752058b42abf787896a8fa29d74777bcb997e
Aptivi <eoflaoevicecity@gmail.com>
2024-10-13 13:00:56 +0300

    add - Added emoji generator (pt. 1)
    ---
    
    We've added a generator that adds support for all Unicode emojis.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Textify.Data/Unicode/Emoji.cs
@@ -0,0 +1,56 @@
+﻿//
+// Textify  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Textify
+//
+// Textify is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Textify is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Textify.Data.Unicode
+{
+    /// <summary>
+    /// Emoji information
+    /// </summary>
+    public class Emoji
+    {
+        private readonly string name = "";
+        private readonly string sequence = "";
+        private readonly EmojiStatus status = EmojiStatus.Component;
+
+        /// <summary>
+        /// Emoji name
+        /// </summary>
+        public string Name =>
+            name;
+
+        /// <summary>
+        /// A sequence of characters that represent an emoji
+        /// </summary>
+        public string Sequence =>
+            sequence;
+
+        /// <summary>
+        /// Emoji status
+        /// </summary>
+        public EmojiStatus Status =>
+            status;
+
+        internal Emoji(string name, string sequence, EmojiStatus status)
+        {
+            this.name = name;
+            this.sequence = sequence;
+            this.status = status;
+        }
+    }
+}
---
+++ Textify.Data/Unicode/EmojiStatus.cs
@@ -0,0 +1,48 @@
+﻿//
+// Textify  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Textify
+//
+// Textify is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Textify is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Textify.Data.Unicode
+{
+    /// <summary>
+    /// Emoji status enumeration
+    /// </summary>
+    public enum EmojiStatus
+    {
+        /// <summary>
+        /// Emoji doesn't have a status
+        /// </summary>
+        None,
+        /// <summary>
+        /// Emoji is a component (excluding Regional_Indicators, ASCII, and non-Emoji.)
+        /// </summary>
+        Component,
+        /// <summary>
+        /// Emoji is fully qualified
+        /// </summary>
+        FullyQualified,
+        /// <summary>
+        /// Emoji is minimally qualified
+        /// </summary>
+        MinimalQualified,
+        /// <summary>
+        /// Emoji is not qualified
+        /// </summary>
+        NotQualified,
+    }
+}
