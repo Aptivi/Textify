239c8e687eff5ce1ec27109edd259fdacf0c4355
Aptivi <eoflaoevicecity@gmail.com>
2024-11-17 08:11:33 +0300

    add - doc - Added more regex tools
    ---
    
    We've added more regular expression tools from Nitrocid KS
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Backport Required: False
    Part: 1/1

---
+++ Textify.Tests/General/RegexToolsTest.cs
@@ -0,0 +1,224 @@
+﻿//
+// Textify  Copyright (C) 2023-2024  Aptivi
+//
+// This file is part of Textify
+//
+// Textify is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Textify is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Text.RegularExpressions;
+using Textify.Tools;
+
+namespace Textify.Tests.General
+{
+    [TestClass]
+    public class RegexToolsTest
+    {
+        [TestMethod]
+        [DataRow(@"^[\w.-]+$", true)]
+        [DataRow(@"", true)]
+        [DataRow("^[\\w.-\"]+$", false)]
+        [Description("Action")]
+        public void TestIsValidRegex(string pattern, bool expected)
+        {
+            bool actual = RegexTools.IsValidRegex(pattern);
+            actual.ShouldBe(expected);
+        }
+
+        [TestMethod]
+        [DataRow("twitch", @"^[\w.-]+$", true)]
+        [DataRow("twi?tch", @"^[\w.-]+$", false)]
+        [DataRow("twitch", "", true)]
+        [DataRow("", @"^[\w.-]+$", false)]
+        [DataRow("", "", true)]
+        [Description("Action")]
+        public void TestIsMatch(string text, string pattern, bool expected)
+        {
+            bool actual = RegexTools.IsMatch(text, pattern);
+            actual.ShouldBe(expected);
+        }
+
+        [TestMethod]
+        [DataRow("twitch", @"^[\w.-]+$")]
+        [DataRow("twi?tch", @"^[\w.-]+$")]
+        [DataRow("twitch", "")]
+        [DataRow("", @"^[\w.-]+$")]
+        [DataRow("", "")]
+        [Description("Action")]
+        public void TestMatch(string text, string pattern)
+        {
+            Match? match = default;
+            Should.NotThrow(() => match = RegexTools.Match(text, pattern));
+        }
+
+        [TestMethod]
+        [DataRow("twitch", "^[\\w.-\"]+$")]
+        [DataRow("twi?tch", "^[\\w.-\"]+$")]
+        [DataRow("", "^[\\w.-\"]+$")]
+        [Description("Action")]
+        public void TestMatchInvalid(string text, string pattern)
+        {
+            Match? match = default;
+            Should.Throw(() => match = RegexTools.Match(text, pattern), typeof(ArgumentException));
+        }
+
+        [TestMethod]
+        [DataRow("twitch", @"^[\w.-]+$")]
+        [DataRow("twi?tch", @"^[\w.-]+$")]
+        [DataRow("twitch", "")]
+        [DataRow("", @"^[\w.-]+$")]
+        [DataRow("", "")]
+        [Description("Action")]
+        public void TestMatches(string text, string pattern)
+        {
+            MatchCollection? match = default;
+            Should.NotThrow(() => match = RegexTools.Matches(text, pattern));
+        }
+
+        [TestMethod]
+        [DataRow("twitch", "^[\\w.-\"]+$")]
+        [DataRow("twi?tch", "^[\\w.-\"]+$")]
+        [DataRow("", "^[\\w.-\"]+$")]
+        [Description("Action")]
+        public void TestMatchesInvalid(string text, string pattern)
+        {
+            MatchCollection? match = default;
+            Should.Throw(() => match = RegexTools.Matches(text, pattern), typeof(ArgumentException));
+        }
+
+        [TestMethod]
+        [DataRow("twitch", @"^[\w.-]+$", "")]
+        [DataRow("twi?tch", @"^[\w.-]+$", "twi?tch")]
+        [DataRow("twitch", "", "twitch")]
+        [DataRow("", @"^[\w.-]+$", "")]
+        [DataRow("", "", "")]
+        [Description("Action")]
+        public void TestFilter(string text, string pattern, string expected)
+        {
+            string? filtered = default;
+            Should.NotThrow(() => filtered = RegexTools.Filter(text, pattern));
+            filtered.ShouldBe(expected);
+        }
+
+        [TestMethod]
+        [DataRow("twitch", "^[\\w.-\"]+$")]
+        [DataRow("twi?tch", "^[\\w.-\"]+$")]
+        [DataRow("", "^[\\w.-\"]+$")]
+        [Description("Action")]
+        public void TestFilterInvalid(string text, string pattern)
+        {
+            string? filtered = default;
+            Should.Throw(() => filtered = RegexTools.Filter(text, pattern), typeof(ArgumentException));
+        }
+
+        [TestMethod]
+        [DataRow("twitch", @"^[\w.-]+$", "switch", "switch")]
+        [DataRow("twi?tch", @"^[\w.-]+$", "switch", "twi?tch")]
+        [DataRow("twitch", "", "switch", "switchtswitchwswitchiswitchtswitchcswitchhswitch")]
+        [DataRow("", @"^[\w.-]+$", "switch", "")]
+        [DataRow("", "", "switch", "switch")]
+        [Description("Action")]
+        public void TestFilter(string text, string pattern, string replaceWith, string expected)
+        {
+            string? filtered = default;
+            Should.NotThrow(() => filtered = RegexTools.Filter(text, pattern, replaceWith));
+            filtered.ShouldBe(expected);
+        }
+
+        [TestMethod]
+        [DataRow("twitch", "^[\\w.-\"]+$", "switch")]
+        [DataRow("twi?tch", "^[\\w.-\"]+$", "switch")]
+        [DataRow("", "^[\\w.-\"]+$", "switch")]
+        [Description("Action")]
+        public void TestFilterInvalid(string text, string pattern, string replaceWith)
+        {
+            string? filtered = default;
+            Should.Throw(() => filtered = RegexTools.Filter(text, pattern, replaceWith), typeof(ArgumentException));
+        }
+
+        [TestMethod]
+        [DataRow("twitch-switch", @"[st]", new string[] { "", "wi", "ch-", "wi", "ch" })]
+        [DataRow("twi?tch-switch", @"[st]", new string[] { "", "wi?", "ch-", "wi", "ch" })]
+        [DataRow("twitch", "", new string[] { "", "t", "w", "i", "t", "c", "h", "" })]
+        [DataRow("", @"[st]", new string[] { "" })]
+        [DataRow("", "", new string[] { "", "" })]
+        [Description("Action")]
+        public void TestSplit(string text, string pattern, string[] expected)
+        {
+            string[]? split = default;
+            Should.NotThrow(() => split = RegexTools.Split(text, pattern));
+            split.ShouldBe(expected);
+        }
+
+        [TestMethod]
+        [DataRow("twitch", "^[\\w.-\"]+$")]
+        [DataRow("twi?tch", "^[\\w.-\"]+$")]
+        [DataRow("", "^[\\w.-\"]+$")]
+        [Description("Action")]
+        public void TestSplitInvalid(string text, string pattern)
+        {
+            string[]? split = default;
+            Should.Throw(() => split = RegexTools.Split(text, pattern), typeof(ArgumentException));
+        }
+
+        [TestMethod]
+        [DataRow(@"Twi\tch", @"Twi\\tch")]
+        [DataRow(@"Twi*tch", @"Twi\*tch")]
+        [DataRow(@"Twi+tch", @"Twi\+tch")]
+        [DataRow(@"Twi?tch", @"Twi\?tch")]
+        [DataRow(@"Twi|tch", @"Twi\|tch")]
+        [DataRow(@"Twi(tch", @"Twi\(tch")]
+        [DataRow(@"Twi{tch", @"Twi\{tch")]
+        [DataRow(@"Twi[tch", @"Twi\[tch")]
+        [DataRow(@"Twi)tch", @"Twi\)tch")]
+        [DataRow(@"Twi^tch", @"Twi\^tch")]
+        [DataRow(@"Twi$tch", @"Twi\$tch")]
+        [DataRow(@"Twi.tch", @"Twi\.tch")]
+        [DataRow(@"Twi#tch", @"Twi\#tch")]
+        [DataRow(@"Twi tch", @"Twi\ tch")]
+        [Description("Action")]
+        public void TestEscape(string text, string expected)
+        {
+            string? final = default;
+            Should.NotThrow(() => final = RegexTools.Escape(text));
+            final.ShouldBe(expected);
+        }
+
+        [TestMethod]
+        [DataRow(@"Twi\\tch", @"Twi\tch")]
+        [DataRow(@"Twi\*tch", @"Twi*tch")]
+        [DataRow(@"Twi\+tch", @"Twi+tch")]
+        [DataRow(@"Twi\?tch", @"Twi?tch")]
+        [DataRow(@"Twi\|tch", @"Twi|tch")]
+        [DataRow(@"Twi\(tch", @"Twi(tch")]
+        [DataRow(@"Twi\{tch", @"Twi{tch")]
+        [DataRow(@"Twi\[tch", @"Twi[tch")]
+        [DataRow(@"Twi\)tch", @"Twi)tch")]
+        [DataRow(@"Twi\^tch", @"Twi^tch")]
+        [DataRow(@"Twi\$tch", @"Twi$tch")]
+        [DataRow(@"Twi\.tch", @"Twi.tch")]
+        [DataRow(@"Twi\#tch", @"Twi#tch")]
+        [DataRow(@"Twi\ tch", @"Twi tch")]
+        [Description("Action")]
+        public void TestUnescape(string text, string expected)
+        {
+            string? final = default;
+            Should.NotThrow(() => final = RegexTools.Unescape(text));
+            final.ShouldBe(expected);
+        }
+    }
+}
--- Textify/Tools/RegexTools.cs
+++ Textify/Tools/RegexTools.cs
@@ -20,6 +20,7 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.RegularExpressions;
+using Textify.General;
 
 namespace Textify.Tools
 {
@@ -28,6 +29,9 @@ namespace Textify.Tools
     /// </summary>
     public static class RegexTools
     {
+        private static string[] escaped = [@"\", @"*", @"+", @"?", @"|", @"{", @"[", @"(", @")", @"^", @"$", @".", @"#", @" ", @"-", @"""", @"'", @"`"];
+        private static string[] unescaped = [@"\\", @"\*", @"\+", @"\?", @"\|", @"\{", @"\[", @"\(", @"\)", @"\^", @"\$", @"\.", @"\#", @"\ ", @"\-", @"\""", @"\'", @"\`"];
+
         /// <summary>
         /// Determines whether the specified regular expression pattern is valid or not
         /// </summary>
@@ -85,5 +89,101 @@ namespace Textify.Tools
                 return false;
             }
         }
+
+        /// <summary>
+        /// Checks to see whether the pattern matches
+        /// </summary>
+        /// <param name="text">The text to be matched</param>
+        /// <param name="pattern">Regular expression pattern for matching</param>
+        /// <returns>True if there are matches. Otherwise, false</returns>
+        public static bool IsMatch(string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern)
+        {
+            if (!IsValidRegex(pattern))
+                return false;
+
+            return new Regex(pattern).IsMatch(text);
+        }
+
+        /// <summary>
+        /// Matches the pattern with the text given
+        /// </summary>
+        /// <param name="text">The text to be matched</param>
+        /// <param name="pattern">Regular expression pattern for matching</param>
+        /// <returns>A <see cref="System.Text.RegularExpressions.Match"/> that contains information about the current match</returns>
+        public static Match Match(string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern)
+        {
+            if (!IsValidRegex(pattern))
+                throw new ArgumentException("Invalid regular expression syntax.");
+
+            return new Regex(pattern).Match(text);
+        }
+
+        /// <summary>
+        /// Matches the pattern with the text given
+        /// </summary>
+        /// <param name="text">The text to be matched</param>
+        /// <param name="pattern">Regular expression pattern for matching</param>
+        /// <returns>Collection of <see cref="System.Text.RegularExpressions.Match"/>es that contains information about the current match</returns>
+        public static MatchCollection Matches(string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern)
+        {
+            if (!IsValidRegex(pattern))
+                throw new ArgumentException("Invalid regular expression syntax.");
+
+            return new Regex(pattern).Matches(text);
+        }
+
+        /// <summary>
+        /// Filters the string from the substrings matched by the given pattern
+        /// </summary>
+        /// <param name="text">The text to be processed</param>
+        /// <param name="pattern">Regular expression pattern for replacing</param>
+        /// <returns>Filtered text</returns>
+        public static string Filter(string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern) =>
+            Filter(text, pattern, "");
+
+        /// <summary>
+        /// Filters the string from the substrings matched by the given pattern
+        /// </summary>
+        /// <param name="text">The text to be processed</param>
+        /// <param name="pattern">Regular expression pattern for replacing</param>
+        /// <param name="replaceWith">Replaces the matched substrings with the specified text</param>
+        /// <returns>Filtered text</returns>
+        public static string Filter(string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern, string replaceWith)
+        {
+            if (!IsValidRegex(pattern))
+                throw new ArgumentException("Invalid regular expression syntax.");
+
+            return new Regex(pattern).Replace(text, replaceWith);
+        }
+
+        /// <summary>
+        /// Splits the string using the matched substrings as the delimiters
+        /// </summary>
+        /// <param name="text">The text to be split</param>
+        /// <param name="pattern">Regular expression pattern for splitting</param>
+        /// <returns>Array of strings</returns>
+        public static string[] Split(string text, [StringSyntax(StringSyntaxAttribute.Regex)] string pattern)
+        {
+            if (!IsValidRegex(pattern))
+                throw new ArgumentException("Invalid regular expression syntax.");
+
+            return new Regex(pattern).Split(text);
+        }
+
+        /// <summary>
+        /// Escapes the invalid characters from the string
+        /// </summary>
+        /// <param name="text">The text containing invalid characters to escape</param>
+        /// <returns>Escaped string</returns>
+        public static string Escape(string text) =>
+            text.ReplaceAllRange(escaped, unescaped);
+
+        /// <summary>
+        /// Unescapes the escaped characters from the string
+        /// </summary>
+        /// <param name="text">The text containing escaped characters to unescape</param>
+        /// <returns>Unescaped string</returns>
+        public static string Unescape(string text) =>
+            text.ReplaceAllRange(unescaped, escaped);
     }
 }
